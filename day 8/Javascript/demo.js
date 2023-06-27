cdab8a316ac4bc044f2f37fe5314573b2bb50730
shubham284 <137398150+shubham284@users.noreply.github.com>
2023-06-27 15:39:44 +0530

    Add files via upload

---
+++ day 8/Javascript/demo.js
@@ -0,0 +1,21 @@
+function demoExternalAlert()
+{
+    alert("Relex boys..");
+}
+function demoExternalConfirm()
+{
+    if(confirm("Are you Sure!!"))
+    {
+        alert("yes");
+    }
+    else
+    {
+        alert("no");
+    }
+}
+function demoExternalPrompt()
+{
+    var fname = prompt("Enter Frist Name:");
+    var lname = prompt("Enter Last Name:");
+    alert(fname + " " +lname);
+}
